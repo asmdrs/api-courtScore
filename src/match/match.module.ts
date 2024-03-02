e674d28a8f9b7936f94ebf01d8da53a7d75fc7d8
Amanda Medeiros <amandasmedeiros@outlook.com.br>
2024-03-02 20:51:51 -0300

    feat: Implemented guard on Match Controller

--- src/match/match.module.ts
+++ src/match/match.module.ts
@@ -4,11 +4,12 @@ import { MatchService } from './service/match-service.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MatchEntity } from './models/match.entity';
 import { AthleteEntity } from 'src/athlete/models/athlete.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [TypeOrmModule.forFeature([MatchEntity, AthleteEntity])],
   controllers: [MatchControllerController],
-  providers: [MatchService],
+  providers: [MatchService, JwtService],
   exports: [MatchService]
 })
 export class MatchModule {}
