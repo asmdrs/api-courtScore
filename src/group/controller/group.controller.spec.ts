1ad4f94cfc1358f23c8865a99fd5bba9da50ffbb
Amanda Medeiros <amandasmedeiros@outlook.com.br>
2024-03-02 20:44:32 -0300

    feat: Implemented guard on Group Controller

--- src/group/controller/group.controller.spec.ts
+++ src/group/controller/group.controller.spec.ts
@@ -1,18 +1,67 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GroupController } from './group.controller';
+import { GroupService } from '../service/group.service';
+import { JwtService } from '@nestjs/jwt';
+import { Repository, UpdateResult } from 'typeorm';
+import { of } from 'rxjs';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { GroupEntity } from '../models/group.entity';
 
 describe('GroupController', () => {
   let controller: GroupController;
+  let service: GroupService;
+  let jwtService: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GroupController],
+      providers: [
+        JwtService,
+        {
+          provide: GroupService,
+          useClass: GroupService,
+          useValue:{
+            changeGroup: jest.fn(() => of({} as UpdateResult))
+          }
+        },
+          {
+            provide: getRepositoryToken(GroupEntity),
+            useClass: Repository
+          }
+      ]
     }).compile();
 
     controller = module.get<GroupController>(GroupController);
+    service = module.get<GroupService>(GroupService);
+    jwtService = module.get<JwtService>(JwtService);
+
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create new group', () => {
+    const groupData = 
+    { 
+      id:"123",
+      name: 'Group',
+    };
+    const result = of(groupData);
+    jest.spyOn(service, 'createGroup').mockReturnValue(result);
+
+    expect(controller.createGroup(groupData)).toEqual(result);
+  });
+
+  it('should find all groups ', () => {
+    const groupData = 
+    { 
+      id:"123",
+      name: 'Group',
+    };
+    const result = of(groupData);
+    jest.spyOn(service, 'createGroup').mockReturnValue(result);
+
+    expect(controller.createGroup(groupData)).toEqual(result);
   });
 });
--- src/group/controller/group.controller.ts
+++ src/group/controller/group.controller.ts
@@ -1,28 +1,43 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { DeleteResult, UpdateResult } from 'typeorm';
 import { Group } from '../models/group.interface';
 import { GroupService } from '../service/group.service';
+import { JwtService } from '@nestjs/jwt';
+import { JwtGuard } from '../../auth/guard/jwt.guard';
+import { Roles } from '../../decorators/roles.decorator';
+import { Role } from '../../auth/models/role.enum';
 
 @Controller('group')
 export class GroupController {
-  constructor(private groupService: GroupService ){}
+  constructor(
+    private groupService: GroupService,
+    private readonly jwtService: JwtService
+     ){}
 
+  @UseGuards(JwtGuard)
+  @Roles(Role.MANAGER || Role.ADMIN)
   @Post()
   createGroup(@Body() group: Group): Observable<Group> {
     return this.groupService.createGroup(group);
   }
 
+  @UseGuards(JwtGuard)
+  @Roles(Role.ADMIN)
   @Get()
   findAllGroups(): Observable<Group[]> {
     return this.groupService.findAllGroups();
   }
   
+  @UseGuards(JwtGuard)
+  @Roles(Role.MANAGER || Role.ADMIN)
   @Put(':id')
   changeGroup(@Param('id') id: string, @Body() group: Group): Observable<UpdateResult> {
     return this.groupService.changeGroup(id, group);
   }
 
+  @UseGuards(JwtGuard)
+  @Roles(Role.MANAGER || Role.ADMIN)
   @Delete(':id')
   deleteGroup(@Param('id') id: string): Observable<DeleteResult> {
     return this.groupService.deleteGroup(id);
